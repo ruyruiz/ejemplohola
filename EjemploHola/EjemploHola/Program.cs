f15c03e92b8aadbea9b86e4186375a543c1f5b4c
Ruy Fernan Ruiz Mojica <ruruiz@dnp.gov.co>
2018-08-24 13:08:24 -0500

    arreglo 1

--- EjemploHola/EjemploHola/Program.cs
+++ EjemploHola/EjemploHola/Program.cs
@@ -17,7 +17,7 @@ namespace EjemploHola
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            //mensage
+            //mensaje
         }
     }
 }
