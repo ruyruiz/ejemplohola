9bec08c25197851b542bd58e26340d67af61a5c7
Ruy Fernan Ruiz Mojica <ruruiz@dnp.gov.co>
2019-01-28 15:38:29 -0500

    eesssssssss

--- EjemploHola/EjemploHola/Form33.cs
+++ EjemploHola/EjemploHola/Form33.cs
@@ -10,21 +10,11 @@ using System.Windows.Forms;
 
 namespace EjemploHola
 {
-    public partial class Form1 : Form
+    public partial class Form33 : Form
     {
-        public Form1()
+        public Form33()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
